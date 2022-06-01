5daa45be166207746f24323cf63d621ffb96d5d1
Can <can.mujde@icloud.com>
2022-06-01 21:52:42 +0300

    joystick pack

---
+++ Assets/Joystick Pack/Scripts/Joysticks/FloatingJoystick.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Managers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FloatingJoystick : Joystick
+{
+
+
+    public static FloatingJoystick instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        background.gameObject.SetActive(false);
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
+        background.gameObject.SetActive(true);
+        base.OnPointerDown(eventData);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        background.gameObject.SetActive(false);
+        GameManager.Instance.SavePlayerLastPosition();
+        base.OnPointerUp(eventData);
+    }
+}
